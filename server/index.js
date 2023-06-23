c0c133917276a8f4b7c90bad7760d4be5c0da068
brunol-pereira <brunoleandropereira@hotmail.com>
2023-06-23 14:18:13 -0300

    Connection Mysql

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
 
-app.get('/', (req, res) =>{
-    res.send("Hello Word");
-})
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "0305",
+    database: "crud",
+});
+
+app.get('/', (req, res) => {
+    let SQL = "INSERT INTO games( name, cost, category) VALUES ('GTA  V', '220', 'Ação')";
+
+    db.query(SQL, (err, result) => {
+        console.log(err);
+    })
+});
 
 app.listen(3001, () => {
     console.log("rodando servidor");
